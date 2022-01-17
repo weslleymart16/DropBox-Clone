310f593b0c8cf6905a49d45a6525e0ca345def84
SuehtamCruz <matheus.cruz@mottu.com.br>
2022-01-17 00:18:51 -0300

    save mode

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,25 +1,23 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var formidable = require('formidable')
+var formidable = require("formidable");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
-router.post('/upload', (req, res) => {
-
+router.post("/upload", (req, res) => {
   let form = new formidable.IncomingForm({
-    uploadDir: './upload',
-    keepExtensions: true
-  })
+    uploadDir: "./upload",
+    keepExtensions: true,
+  });
 
   form.parse(req, (err, fields, files) => {
     res.json({
-      files
-    })
-  })
-
-})
+      files,
+    });
+  });
+});
 
 module.exports = router;
